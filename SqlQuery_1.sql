baf46bf46ac487d0cdaf969bdbd2db3bd9de7577
InOutsource <athomas@inoutsource.com>
2016-04-07 12:04:50 -0400

    Updated query

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -1,16 +1,9 @@
-﻿SELECT			'SmartSheet_#02_DataCleanClient'				AS	'SmartSheet_#02_DataCleanClient',
-				HBM_CLIENT.CLIENT_UNO							AS	ClientUno, 
-				HBM_CLIENT.CLIENT_CODE							AS	ClientCode, 
-				HBM_CLIENT.CLIENT_NAME							AS	ClientName, 
-				HBM_CLIENT.OPEN_DATE							AS	OpenOn,
-				HBM_CLIENT.CLOSE_DATE							AS	CloseOn,
-				HBM_CLIENT.STATUS_CODE							AS	StatusCode
-	FROM		[dbo].[HBM_CLIENT]
-	WHERE		LEN(HBM_CLIENT.CLIENT_CODE)						=	1			OR
-				LEN(HBM_CLIENT.CLIENT_CODE)						=	3			OR
-				LEN(HBM_CLIENT.CLIENT_CODE)						=	4			OR
-				LEN(HBM_CLIENT.CLIENT_CODE)						=	6			OR
-				LEN(HBM_CLIENT.CLIENT_CODE)						=	7 
-	ORDER BY
-				LEN(CLIENT_CODE)
-				 
+﻿SELECT			'SmartSheet_#03_DataCleanClientName'		AS			'SmartSheet_#03_DataCleanClientName',
+				hbm_name.[NAME]								AS			ClientName
+	FROM		[dbo].[HBM_CLIENT]							AS			hbm_client
+				INNER
+				JOIN
+				[dbo].[HBM_NAME]							AS			hbm_name
+				ON
+				hbm_client.[NAME_UNO]						=			hbm_name.[NAME_UNO]
+	WHERE		hbm_name.[NAME]								like		'%No New Work%'
